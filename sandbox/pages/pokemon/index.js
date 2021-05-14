619e057d86537c9384e14db6a22777760cb9e1d9
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-05-14 08:00:26 -0400

    passing a prop to another component

--- sandbox/pages/pokemon/index.js
+++ sandbox/pages/pokemon/index.js
@@ -1,3 +1,5 @@
+import Test from "../test/content";
+
 export async function getStaticProps() {
     const req = await fetch(`https://pokedex-alchemy.herokuapp.com/api/pokedex`)
     const pokemon = await req.json();
@@ -18,6 +20,8 @@ export default function PokemonDoesThisHaveToMatch({ pokemon }) {
             <h2>First Pokemon is</h2>
             <h3>{pokemon.results[0].pokemon}</h3>
             <img src={pokemon.results[0].url_image} alt='image of venausaur' width='300' />
+
+            <Test pokeProp={pokemon.results[0]} />
         </>
     )
 }
--- sandbox/pages/test/content.js
+++ sandbox/pages/test/content.js
@@ -1,5 +1,8 @@
-export default function Test() {
+export default function Test({ pokeProp }) {
     return (
-        <p>hello from content</p>
+        <>
+            <p>hello from content</p>
+            <p>This is a prop I passed down from Pokemon Component: {pokeProp.pokemon}</p>
+        </>
     )
 }
