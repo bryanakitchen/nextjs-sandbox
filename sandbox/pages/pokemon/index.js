a973da9f4adfdd80d0dddecd8c0a8d63b88b8c0e
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-05-14 07:56:39 -0400

    using getStaticProps to make a fetch call

---
+++ sandbox/pages/pokemon/index.js
@@ -0,0 +1,23 @@
+export async function getStaticProps() {
+    const req = await fetch(`https://pokedex-alchemy.herokuapp.com/api/pokedex`)
+    const pokemon = await req.json();
+
+    return {
+        props: { pokemon },
+    }
+}
+
+
+export default function PokemonDoesThisHaveToMatch({ pokemon }) {
+    // console.log(pokemon.results[0]);
+
+    return (
+        <>
+            <h1>hello from Pokemon</h1>
+
+            <h2>First Pokemon is</h2>
+            <h3>{pokemon.results[0].pokemon}</h3>
+            <img src={pokemon.results[0].url_image} alt='image of venausaur' width='300' />
+        </>
+    )
+}
