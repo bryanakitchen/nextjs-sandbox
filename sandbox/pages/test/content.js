7a31e723d180050cc4e81e8fc3b40af856e102c3
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-05-14 08:02:14 -0400

    conditional rendering based on if prop exists or not - reusing test component

--- sandbox/pages/test/content.js
+++ sandbox/pages/test/content.js
@@ -2,7 +2,7 @@ export default function Test({ pokeProp }) {
     return (
         <>
             <p>hello from content</p>
-            <p>This is a prop I passed down from Pokemon Component: {pokeProp.pokemon}</p>
+            {!pokeProp ? <p>No prop from this path!</p> : <p>This is a prop I passed down from Pokemon Component: {pokeProp.pokemon}</p>}
         </>
     )
 }
